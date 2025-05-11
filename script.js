2f1bfb5c88b86c0e5d0b10885bf09f875fcf6fef
ShoulderEscape <105722638+ShoulderEscape@users.noreply.github.com>
2025-05-11 20:41:50 +0200

    Adding crit system

--- script.js
+++ script.js
@@ -32,30 +32,74 @@ function roll() {
     case "RLeg":
     case "LLeg": adjustedBase -= 2; break;
   }
+  
 
-  const finalTarget = targetInput === "Random" ? getRandomTarget() : targetInput;
 
   const { result: accuracyResult, rolls: accuracyRolls } = rollToHit();
   const { result: damageResult, rolls: damageRolls } = rollDamage(damageDice);
+  let { result: critResult, rolls: critRolls } = rollCrit();
 
+
+  let finalTarget = targetInput;
+  let critTarget = targetInput;
+
+  if(targetInput === "Random"){
+    
+    finalTarget = getRandomTarget();
+    critTarget = getCritTarget(critResult);
+
+  } else {
+
+    let tempCritTarget;
+    while(tempCritTarget != critTarget){
+
+      ({ result: critResult, rolls: critRolls } = rollCrit());
+      tempCritTarget = getCritTarget(critResult);
+
+    }
+  }
   let totalDamage = (damageResult + baseDamage) * damageMultipliers[finalTarget];
+
+  let critDamage = (damageResult + baseDamage) * damageMultipliers[critTarget]
 
   if (isHeavyAttack) {
     totalDamage *= 2;
+    critDamage *= 2;
     adjustedBase -= 3;
   }
+  
 
   document.getElementById("result-hits").textContent = accuracyResult + adjustedBase;
   document.getElementById("result-hits-rolled").textContent = accuracyRolls.join(" | ");
   document.getElementById("result-damage-rolled").textContent = damageRolls.join(" | ");
-  document.getElementById("result-total").textContent = totalDamage;
+  document.getElementById("result-total").textContent = Math.floor(totalDamage);
   document.getElementById("result-target").textContent = finalTarget;
+  document.getElementById("result-crit-rolled").textContent = critRolls.join(" | ");
+  document.getElementById("result-crit-total").textContent = critResult;
+  document.getElementById("result-crit-target").textContent = critTarget;
+  document.getElementById("result-crit-damage").textContent = critDamage;
+
 }
 
 
 function rollDie(sides) {
   return Math.floor(Math.random() * sides) + 1;
 }
+function rollCrit() {
+  const rolls = [];
+
+  const roll1 = rollDie(6);
+  const roll2 = rollDie(6);
+
+
+  rolls.push(roll1);
+  rolls.push(roll2);
+
+  const total = roll1 + roll2;  
+
+  return { result: total, rolls };
+}
+
 
 function rollToHit() {
   const rolls = [];
@@ -103,4 +147,17 @@ function getRandomTarget() {
   if (roll === 6) return "LArm";
   if (roll === 7 || roll === 8) return "RLeg";
   return "LLeg";
+}
+
+function getCritTarget(dieResult) {
+  if (dieResult === 12 || dieResult === 11) return "Head";
+  if (dieResult >= 6 && dieResult <= 10) return "Torso";
+  if (dieResult === 5 || dieResult === 4) return randomOrientation() + "Arm" 
+  if (dieResult === 2 || dieResult === 3) return randomOrientation() + "Leg";
+}
+
+function randomOrientation(){
+  const roll = rollDie(2);
+  if(roll === 1) return "R"
+  return "L"
 }
